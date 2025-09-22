6202fa9c2729770de77bb916eca962c7f73e4e54
darkhorse1081 <186711485+darkhorse1081@users.noreply.github.com>
2025-09-22 19:12:01 +1200

    load functionalities

--- assignment-1-rgir859/src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ assignment-1-rgir859/src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -9,11 +9,14 @@ public class InsuranceSystem {
     // fields 
   private ArrayList<UserProfile> userDataBase;
   private ArrayList<String> nameVerification;
+  private boolean isProfileLoaded;
+  private int loadPosition = 0;
 
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
      this.userDataBase = new ArrayList<UserProfile>();
      this.nameVerification = new ArrayList<String>();
+     this.isProfileLoaded = false;
   }
 
   public boolean isNumber(String input) {
@@ -27,6 +30,12 @@ public class InsuranceSystem {
       }
     }
 
+  public String convertCap(String userName) {
+    String converted = userName.substring(0, 1).toUpperCase() + 
+         userName.substring(1).toLowerCase();
+    return converted;
+  }
+
   public void printDatabase() {
 
     if (userDataBase.isEmpty()) {
@@ -39,31 +48,35 @@ public class InsuranceSystem {
 
     } else {
         MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(userDataBase.size()),"s",":");
+        for (int i = 0; i < userDataBase.size(); i++) {
+          if (userDataBase.get(i).getProfileLoaded()) {
+            MessageCli.PRINT_DB_PROFILE_HEADER_SHORT.printMessage("***",
+              String.valueOf(i+1),nameVerification.get(i),userDataBase.get(i).getAge());
+          } else {
+              MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(String.valueOf(i+1),
+              nameVerification.get(i), userDataBase.get(i).getAge());            
+          }
 
-      for (int i = 0; i < userDataBase.size(); i++) {
-        MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(String.valueOf(i+1),
-        nameVerification.get(i), userDataBase.get(i).getAge());
-      }
+        }
     }
   }
 
   public void createNewProfile(String userName, String age) {
 
+    if (isProfileLoaded) {
+      MessageCli.CANNOT_CREATE_WHILE_LOADED.printMessage(nameVerification.get(loadPosition));
+    }
     if (userName.length()<3) { 
       MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName); // username incorrect length
-      
-    } else if (nameVerification.contains
-              (userName.substring(0, 1).toUpperCase() + 
-               userName.substring(1).toLowerCase())) {
-              MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName); // username is present in database
+
+    } else if (nameVerification.contains(convertCap(userName))) {
+         MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(convertCap(userName)); // username is present in database
 
     } else if (isNumber(age) == false){ // age input is not a postive integer
         MessageCli.INVALID_AGE.printMessage(age);
-    }
-      else {
+    } else {
         if (Integer.parseInt(age)<=0) { // if age is negative 
           MessageCli.INVALID_AGE.printMessage(age);
-
         } else {
               UserProfile newEntry = new UserProfile(userName, age);
               userDataBase.add(newEntry);
@@ -76,11 +89,41 @@ public class InsuranceSystem {
   }
 
   public void loadProfile(String userName) {
-    // TODO: Complete this method.
+
+    String proccessed = convertCap(userName);
+    if (userName.length()<3) { 
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(proccessed); // username incorrect length
+    } 
+    if (nameVerification.contains(proccessed)) {
+      if (isProfileLoaded == false) { 
+         isProfileLoaded = true;
+         loadPosition = nameVerification.indexOf(proccessed);
+         userDataBase.get(loadPosition).setProfileLoaded(true);
+         MessageCli.PROFILE_LOADED.printMessage(proccessed); // success
+        
+      } else if (isProfileLoaded == true) { 
+         userDataBase.get(loadPosition).setProfileLoaded(false); // case -> p1 already loaded -> p2 loaded 
+         //-> what happens to loadt status of p1 instance
+         loadPosition = nameVerification.indexOf(proccessed);
+         userDataBase.get(loadPosition).setProfileLoaded(true);
+         MessageCli.PROFILE_LOADED.printMessage(proccessed); 
+      }
+
+    } else {
+         MessageCli.NO_PROFILE_FOUND_TO_LOAD.printMessage(proccessed);
+    }
+
   }
 
   public void unloadProfile() {
-    // TODO: Complete this method.
+
+    if (isProfileLoaded == true) { 
+      isProfileLoaded = false;
+      userDataBase.get(loadPosition).setProfileLoaded(false);
+      MessageCli.PROFILE_UNLOADED.printMessage(userDataBase.get(loadPosition).getFirstName());
+    } else {
+      MessageCli.NO_PROFILE_LOADED.printMessage();
+    }
   }
 
   public void deleteProfile(String userName) {
--- assignment-1-rgir859/src/main/java/nz/ac/auckland/se281/UserProfile.java
+++ assignment-1-rgir859/src/main/java/nz/ac/auckland/se281/UserProfile.java
@@ -4,11 +4,13 @@ public class UserProfile {
      
   private String userName = "";
   private String age = "";
+  private boolean profileLoaded;
 
   public UserProfile(String userName, String age) {
     this.userName = userName.substring(0, 1).toUpperCase() + 
     userName.substring(1).toLowerCase();
     this.age = age;
+    this.profileLoaded = false;
   }
 
   public String getFirstName() {
@@ -17,6 +19,14 @@ public class UserProfile {
 
   public String getAge() {
     return this.age;
+  }
+
+  public void setProfileLoaded(boolean flag) {
+    this.profileLoaded = flag;
+  }
+
+  public boolean getProfileLoaded() {
+    return this.profileLoaded;
   }
 
 }
