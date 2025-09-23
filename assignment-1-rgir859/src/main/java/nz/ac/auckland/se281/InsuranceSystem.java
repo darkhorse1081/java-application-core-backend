f52f046eeeff92481d056f3182a7a06d95fd942c
darkhorse1081 <186711485+darkhorse1081@users.noreply.github.com>
2025-09-23 20:39:31 +1200

    Update InsuranceSystem.java

--- assignment-1-rgir859/src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ assignment-1-rgir859/src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -42,10 +42,14 @@ public class InsuranceSystem {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(userDataBase.size()),"s",".");
 
     } else if (userDataBase.size() == 1) {
-        MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(userDataBase.size()),"",":");
-        MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(String.valueOf(userDataBase.size()),
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(userDataBase.size()),"",":");
+      if (isProfileLoaded == true) {
+          MessageCli.PRINT_DB_PROFILE_HEADER_SHORT.printMessage("*** ",
+          String.valueOf(userDataBase.size()),nameVerification.get(0),userDataBase.get(0).getAge());
+      } else {
+          MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(String.valueOf(userDataBase.size()),
           nameVerification.get(0), userDataBase.get(0).getAge());
-
+      }
     } else {
         MessageCli.PRINT_DB_POLICY_COUNT.printMessage(String.valueOf(userDataBase.size()),"s",":");
         for (int i = 0; i < userDataBase.size(); i++) {
@@ -65,27 +69,25 @@ public class InsuranceSystem {
 
     if (isProfileLoaded == true) {
       MessageCli.CANNOT_CREATE_WHILE_LOADED.printMessage(nameVerification.get(loadPosition));
-    }
-    if (userName.length()<3) { 
-      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName); // username incorrect length
-
-    } else if (nameVerification.contains(convertCap(userName))) {
-         MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(convertCap(userName)); // username is present in database
-
-    } else if (isNumber(age) == false){ // age input is not a postive integer
-        MessageCli.INVALID_AGE.printMessage(age);
     } else {
-        if (Integer.parseInt(age)<=0) { // if age is negative 
-          MessageCli.INVALID_AGE.printMessage(age);
+        if (userName.length()<3) { 
+          MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName); // username incorrect length
+        } else if (nameVerification.contains(convertCap(userName))) {
+            MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(convertCap(userName)); // username is present in database
+        } else if (isNumber(age) == false){ // age input is not a postive integer
+            MessageCli.INVALID_AGE.printMessage(age);
         } else {
-              UserProfile newEntry = new UserProfile(userName, age);
-              userDataBase.add(newEntry);
-              nameVerification.add(newEntry.getFirstName());
-              MessageCli.PROFILE_CREATED.printMessage(newEntry.getFirstName(), 
-              newEntry.getAge());
-            }
-    }
-
+            if (Integer.parseInt(age)<=0) { // if age is negative 
+              MessageCli.INVALID_AGE.printMessage(age);
+            } else {
+                  UserProfile newEntry = new UserProfile(userName, age);
+                  userDataBase.add(newEntry);
+                  nameVerification.add(newEntry.getFirstName());
+                  MessageCli.PROFILE_CREATED.printMessage(newEntry.getFirstName(), 
+                  newEntry.getAge());
+                }
+              }
+            } 
   }
 
   public void loadProfile(String userName) {
@@ -93,23 +95,24 @@ public class InsuranceSystem {
     String proccessed = convertCap(userName);
     if (userName.length()<3) { 
         MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(proccessed); // username incorrect length
-    } 
-    if (nameVerification.contains(proccessed)) {
-      if (isProfileLoaded == false) { // loading valid profile when nothing is loaded
-         isProfileLoaded = true;
-         loadPosition = nameVerification.indexOf(proccessed);
-         userDataBase.get(loadPosition).setProfileLoaded(true);
-         MessageCli.PROFILE_LOADED.printMessage(proccessed); // success
-        
-      } else if (isProfileLoaded == true) { 
-         userDataBase.get(loadPosition).setProfileLoaded(false); // case -> p1 already loaded -> p2 loaded 
-         //-> what happens to loadt status of p1 instance
-         loadPosition = nameVerification.indexOf(proccessed); // updated load index position
-         userDataBase.get(loadPosition).setProfileLoaded(true);
-         MessageCli.PROFILE_LOADED.printMessage(proccessed); 
-      } 
-    } else {
-        MessageCli.NO_PROFILE_FOUND_TO_LOAD.printMessage(proccessed);
+    } else{
+        if (nameVerification.contains(proccessed)) {
+          if (isProfileLoaded == false) { // loading valid profile when nothing is loaded
+            isProfileLoaded = true;
+            loadPosition = nameVerification.indexOf(proccessed);
+            userDataBase.get(loadPosition).setProfileLoaded(true);
+            MessageCli.PROFILE_LOADED.printMessage(proccessed); // success
+            
+          } else if (isProfileLoaded == true) { 
+            userDataBase.get(loadPosition).setProfileLoaded(false); // case -> p1 already loaded -> p2 loaded 
+            //-> what happens to loadt status of p1 instance
+            loadPosition = nameVerification.indexOf(proccessed); // updated load index position
+            userDataBase.get(loadPosition).setProfileLoaded(true);
+            MessageCli.PROFILE_LOADED.printMessage(proccessed); 
+          } 
+        } else {
+            MessageCli.NO_PROFILE_FOUND_TO_LOAD.printMessage(proccessed);
+        }
     }
 
   }
@@ -131,25 +134,23 @@ public class InsuranceSystem {
       MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(convertCap(userName));
     } else {
         if (nameVerification.contains(convertCap(userName))) {
-           int x = nameVerification.indexOf(convertCap(userName));
-           if ((x == loadPosition) && (isProfileLoaded == true)) { // current profile loaded is being deleted error
+            int x = nameVerification.indexOf(convertCap(userName));
+            if ((x == loadPosition) && (isProfileLoaded == true)) { // current profile loaded is being deleted error
                 MessageCli.CANNOT_DELETE_PROFILE_WHILE_LOADED.printMessage(convertCap(userName));
-           } else {
-                nameVerification.remove(x); // nothing is loaded by default
-                userDataBase.remove(x); // -> shift indexing of name amd userdatabase 
-                // what happens to isProfileLoaded + loadPosition -> [reset]
-                // new load position AFTER deletion
-                if (isProfileLoaded == true) { // position shift of loaded if non loaded is deleted
+            } else {
+                nameVerification.remove(x);       // nothing is loaded by default
+                userDataBase.remove(x);           // shift indexing of name amd userdatabase 
+                if (isProfileLoaded == true) {    // position shift of loaded if non loaded is deleted
                     for (int i = 0; i < userDataBase.size(); i++) {
                       if (userDataBase.get(i).getProfileLoaded() == true) {
                           loadPosition = i;
+                      }
                     }
-                  }
                 }
                 MessageCli.PROFILE_DELETED.printMessage(convertCap(userName));
-           }
+              }
         } else {
-           MessageCli.NO_PROFILE_FOUND_TO_DELETE.printMessage(convertCap(userName));
+            MessageCli.NO_PROFILE_FOUND_TO_DELETE.printMessage(convertCap(userName));
         }
     }
   }
